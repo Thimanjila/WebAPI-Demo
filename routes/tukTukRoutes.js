6472312eea06938e0fe5c9472ba7e7242fffabbd
Thimanjila <tdudangawe@gmail.com>
2026-05-02 20:08:01 +0530

    add swagger docs

--- routes/tukTukRoutes.js
+++ routes/tukTukRoutes.js
@@ -6,7 +6,42 @@ const {
     getTukTuks
 } = require("../controllers/tukTukController");
 
-router.post("/", createTukTuk);
+/**
+ * @openapi
+ * /api/tuktuks:
+ *   get:
+ *     summary: Get all TukTuks
+ *     responses:
+ *       200:
+ *         description: List of TukTuks
+ */
 router.get("/", getTukTuks);
+
+/**
+ * @openapi
+ * /api/tuktuks:
+ *   post:
+ *     summary: Create a TukTuk
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               vehicleNumber:
+ *                 type: string
+ *                 example: WP CAB 1234
+ *               driverName:
+ *                 type: string
+ *                 example: Kamal
+ *               deviceId:
+ *                 type: string
+ *                 example: DEV001
+ *     responses:
+ *       201:
+ *         description: TukTuk created successfully
+ */
+router.post("/", createTukTuk);
 
 module.exports = router;
