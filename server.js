750bf7fdc3d0dac197de8a3b7a1edf96243ca4c3
Thimanjila <tdudangawe@gmail.com>
2026-05-02 19:36:28 +0530

    add swagger docs

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
-
-
 const tukTukRoutes = require("./routes/tukTukRoutes");
+
+// Swagger imports
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 const app = express();
 
@@ -15,18 +17,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Swagger config
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "TukTuk API",
+      version: "1.0.0"
+    }
+  },
+  apis: ["./routes/*.js"]
+};
 
+const swaggerSpec = swaggerJsDoc(options);
+
+// Swagger route
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// routes
 app.use("/api/tuktuks", tukTukRoutes);
-
 app.use("/api/location", locationRoutes);
-
-// connect database
-connectDB();
 
 // test route
 app.get("/", (req, res) => {
     res.send("Tuk Tuk API is running...");
 });
+
+// connect database
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
