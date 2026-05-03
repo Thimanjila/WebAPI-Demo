672721e7075c40875877c4ecbac05c35a10959bb
Thimanjila <tdudangawe@gmail.com>
2026-05-03 14:44:42 +0530

    add swagger for location routes

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,26 +1,74 @@
 const express = require("express");
 const router = express.Router();
-const { filterLocations } = require("../controllers/locationController");
 
 const {
     addLocation,
-    getLocationsByTukTuk
+    getLocationsByTukTuk,
+    filterLocations
 } = require("../controllers/locationController");
 
 // Auth Middleware
 const authMiddleware = require("../middleware/auth");
 
-// POST GPS data
+/**
+ * @openapi
+ * /api/location:
+ *   post:
+ *     summary: Add GPS location for a TukTuk
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               tukTukId:
+ *                 type: string
+ *                 example: 69efb982b4e240f300132173
+ *               latitude:
+ *                 type: number
+ *                 example: 7.2906
+ *               longitude:
+ *                 type: number
+ *                 example: 80.6337
+ *     responses:
+ *       201:
+ *         description: Location added successfully
+ */
 router.post("/", authMiddleware, addLocation);
-//  
+
+/**
+ * @openapi
+ * /api/location:
+ *   get:
+ *     summary: Get all / filtered locations
+ *     responses:
+ *       200:
+ *         description: List of locations
+ */
 router.get("/", filterLocations);
 
-// GET history
+/**
+ * @openapi
+ * /api/location/{tukTukId}:
+ *   get:
+ *     summary: Get location history for a TukTuk
+ *     parameters:
+ *       - in: path
+ *         name: tukTukId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Location history
+ */
 router.get("/:tukTukId", getLocationsByTukTuk);
 
-
-
-
-
-
 module.exports = router;
+
+
+
+
+
+
